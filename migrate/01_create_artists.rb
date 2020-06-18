b7ecd2a36c708952f49627d6d3e6740c6f2fc13e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-18 20:48:59 +0000

    Automatically backed up by Learn

--- migrate/01_create_artists.rb
+++ migrate/01_create_artists.rb
@@ -6,7 +6,10 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   end
 end
 
-
+def change
+  create_table :artists do |t|
+  end
+end
 
 #create artists migration will generate our artists table with the appropriate columns
 
